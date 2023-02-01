adb8558155a418d1a24a4f9ce942f60ce25d821e
Andy <andy@openformat.tech>
2023-02-01 14:39:36 +0000

    update compatible chains in react package

--- packages/react/src/wagmi-client.ts
+++ packages/react/src/wagmi-client.ts
@@ -6,7 +6,7 @@ import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [chain.polygonMumbai],
+  [chain.localhost, chain.polygonMumbai, chain.polygon],
   [publicProvider()]
 );
 
