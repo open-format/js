50c482ec0c7f77f6217e28cf8a39cd069cb2500b
Andy <andy@openformat.tech>
2023-02-01 14:32:33 +0000

    Add mint, burn, ownerOf and transfer SDK logic

--- packages/sdk/src/core/app.ts
+++ packages/sdk/src/core/app.ts
@@ -1,7 +1,4 @@
 import { providers, Signer } from 'ethers';
-import { Loyalty } from './application/Loyalty';
-import { Marketplace } from './application/Marketplace';
-import { Tickets } from './application/Tickets';
 import { BaseContract } from './base';
 import { ERC20 } from './token/ERC20';
 import { ERC721 } from './token/ERC721';
@@ -13,20 +10,23 @@ import { ERC721 } from './token/ERC721';
  */
 export class App extends BaseContract {
   address: string;
-  ERC20: ERC20;
-  ERC721: ERC721;
-  Marketplace: Marketplace;
-  Loyalty: Loyalty;
-  Tickets: Tickets;
 
   constructor(address: string, provider: providers.Provider, signer?: Signer) {
     super(provider, signer);
     this.address = address;
     this.signer = signer;
-    this.ERC20 = new ERC20();
-    this.ERC721 = new ERC721();
-    this.Marketplace = new Marketplace(address);
-    this.Loyalty = new Loyalty();
-    this.Tickets = new Tickets();
+  }
+
+  getNFT(address: string) {
+    if (!this.signer) return 'No signer found';
+    // GO FIND ADDRESS IN SUBGRAPH
+    return new ERC721(this.provider, address, this.signer);
+  }
+
+  getToken() {
+    // GO FIND ADDRESS IN SUBGRAPH
+    // const address = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+    const nft: ERC20 = new ERC20();
+    return nft;
   }
 }
--- packages/sdk/src/core/token/ERC721.ts
+++ packages/sdk/src/core/token/ERC721.ts
@@ -1,11 +1,75 @@
-export class ERC721 {
-  async deploy() {
-    return 'deploy()';
+import { ethers, providers, Signer } from 'ethers';
+import { ERC721Base } from '../../contract-types';
+import { ERC721Base__factory } from '../../contract-types/factories/token/ERC721';
+import { invariant } from '../../helpers/invariant';
+
+import { BaseContract } from '../base';
+
+export class ERC721 extends BaseContract {
+  contract: ERC721Base | undefined;
+
+  constructor(provider: providers.Provider, address: string, signer: Signer) {
+    super(provider, signer);
+
+    if (address && ethers.utils.isAddress(address)) {
+      this.contract = ERC721Base__factory.connect(address, signer);
+    } else {
+      throw new Error('Invalid Address');
+    }
   }
-  async mint() {
-    return 'mint()';
+
+  async mint(to: string) {
+    invariant(this.contract, 'Invalid contract');
+
+    if (this.contract) {
+      const tx = await this.contract.mintTo(to);
+      const receipt = tx.wait();
+      return receipt;
+    }
+
+    return;
   }
-  async transfer() {
-    return 'transfer()';
+
+  async burn(tokenId: ethers.BigNumberish) {
+    invariant(this.contract, 'Invalid contract');
+
+    if (this.contract) {
+      const tx = await this.contract.burn(tokenId);
+      const receipt = tx.wait();
+      return receipt;
+    }
+
+    return;
+  }
+
+  async ownerOf(tokenId: ethers.BigNumberish): Promise<string | undefined> {
+    invariant(this.contract, 'Invalid contract');
+    if (this.contract) {
+      try {
+        const owner = await this.contract.ownerOf(tokenId);
+        return owner.toString();
+      } catch (e) {
+        return `Error: ${e}`;
+      }
+    }
+    return;
+  }
+
+  async transfer(from: string, to: string, tokenId: ethers.BigNumberish) {
+    invariant(this.contract, 'Invalid contract');
+    if (this.contract) {
+      try {
+        const tx = await this.contract.transferFrom(from, to, tokenId);
+        const receipt = tx.wait();
+        return receipt;
+      } catch (e) {
+        return `Error: ${e}`;
+      }
+    }
+    return;
+  }
+
+  async getAll() {
+    return 'getAll()';
   }
 }
