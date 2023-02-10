735502b5d5e2780c7aa412596573652812154607
Andy <andy@openformat.tech>
2023-02-10 17:16:33 +0000

    Add subgraph and polling to ERC721 class

---
+++ packages/sdk/src/core/subgraph.ts
@@ -0,0 +1,76 @@
+import { gql, request, RequestDocument, Variables } from 'graphql-request';
+import { AppResponse, ContractResponse } from '../types';
+
+//@TODO: This needs to be updated depending on chain
+const ENDPOINT = 'http://0.0.0.0:8000/subgraphs/name/open-format/mumbai';
+
+/**
+ * Makes a raw request to the Open Format subgraph allowing you to pass your own
+ * Query or Mutation
+ * @param document
+ * @returns a result from the subgraph
+ *
+ * @example
+ * ```
+ * rawRequest(gql`{ apps { id } }`)
+ * ```
+ */
+export async function rawRequest<T = any, V = Variables>(
+  document: RequestDocument,
+  variables?: V
+) {
+  return await request<T, V>(ENDPOINT, document, variables);
+}
+
+export async function getAppIdsByCreator({ creator }: { creator: string }) {
+  const query = gql`
+    query getAppIdsByCreator($creator: String) {
+      apps(
+        where: { creator: $creator }
+        orderBy: createdAt
+        orderDirection: desc
+      ) {
+        id
+      }
+    }
+  `;
+
+  return await request<AppResponse, { creator: string }>(ENDPOINT, query, {
+    creator: creator,
+  });
+}
+
+export async function getERC721ByCreator({
+  appId,
+  createdAt,
+}: {
+  appId: string;
+  createdAt: string;
+}) {
+  const query = gql`
+    query getERC721ContractByApp($appId: String!, $createdAt: String!) {
+      contracts(
+        where: { app: $appId, type: "ERC721", createdAt: $createdAt }
+        orderBy: createdAt
+        orderDirection: desc
+      ) {
+        id
+        type
+        createdAt
+        creator
+        app {
+          id
+        }
+      }
+    }
+  `;
+
+  return await request<ContractResponse, { appId: string; createdAt: string }>(
+    ENDPOINT,
+    query,
+    {
+      appId: appId,
+      createdAt: createdAt,
+    }
+  );
+}
--- packages/sdk/src/core/token/ERC721.ts
+++ packages/sdk/src/core/token/ERC721.ts
@@ -1,75 +1,43 @@
-import { ethers, providers, Signer } from 'ethers';
-import { ERC721Base } from '../../contract-types';
-import { ERC721Base__factory } from '../../contract-types/factories/token/ERC721';
-import { invariant } from '../../helpers/invariant';
-
+import { providers, Signer } from 'ethers';
+import { ERC721Factory__factory } from '../../contract-types';
+import { poll } from '../../helpers/subgraph';
+import { ContractResponse } from '../../types';
 import { BaseContract } from '../base';
+import { getERC721ByCreator } from '../subgraph';
 
 export class ERC721 extends BaseContract {
-  contract: ERC721Base | undefined;
-
   constructor(provider: providers.Provider, address: string, signer: Signer) {
     super(provider, address, signer);
-
-    if (address && ethers.utils.isAddress(address)) {
-      this.contract = ERC721Base__factory.connect(address, signer);
-    } else {
-      throw new Error('Invalid Address');
-    }
   }
 
-  async mint(to: string) {
-    invariant(this.contract, 'Invalid contract');
+  async create(
+    name: string,
+    symbol: string,
+    address: string,
+    royaltyBps: number
+  ) {
+    if (!this.signer) return;
 
-    if (this.contract) {
-      const tx = await this.contract.mintTo(to);
-      const receipt = tx.wait();
-      return receipt;
-    }
+    const contract = ERC721Factory__factory.connect(this.appId, this.signer);
+    const tx = await contract.createERC721(name, symbol, address, royaltyBps);
+    const receipt = await tx.wait();
+    const txTimestamp = (await this.provider.getBlock(receipt.blockNumber))
+      .timestamp;
 
-    return;
-  }
+    const subgraphCall = async () =>
+      await getERC721ByCreator({
+        appId: this.appId.toString(),
+        createdAt: txTimestamp.toString(),
+      });
 
-  async burn(tokenId: ethers.BigNumberish) {
-    invariant(this.contract, 'Invalid contract');
+    const validate = (response: ContractResponse) =>
+      !!response.contracts.length;
 
-    if (this.contract) {
-      const tx = await this.contract.burn(tokenId);
-      const receipt = tx.wait();
-      return receipt;
-    }
-
-    return;
-  }
-
-  async ownerOf(tokenId: ethers.BigNumberish): Promise<string | undefined> {
-    invariant(this.contract, 'Invalid contract');
-    if (this.contract) {
-      try {
-        const owner = await this.contract.ownerOf(tokenId);
-        return owner.toString();
-      } catch (e) {
-        return `Error: ${e}`;
-      }
-    }
-    return;
-  }
-
-  async transfer(from: string, to: string, tokenId: ethers.BigNumberish) {
-    invariant(this.contract, 'Invalid contract');
-    if (this.contract) {
-      try {
-        const tx = await this.contract.transferFrom(from, to, tokenId);
-        const receipt = tx.wait();
-        return receipt;
-      } catch (e) {
-        return `Error: ${e}`;
-      }
-    }
-    return;
-  }
-
-  async getAll() {
-    return 'getAll()';
+    return await poll({
+      fn: subgraphCall,
+      validate,
+      interval: 1000,
+      maxAttempts: 20,
+    }).then((response) => (response as ContractResponse).contracts[0].id);
   }
 }
