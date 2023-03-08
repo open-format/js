2312a8e2ee07041a7e61ee4ff35105611dbfdd8f
Andy <andy@openformat.tech>
2023-03-08 12:50:08 +0000

    [sdk] update parseErrorData helper and validateWalletAndMetadata function

--- packages/sdk/src/helpers/transaction.ts
+++ packages/sdk/src/helpers/transaction.ts
@@ -1,5 +1,8 @@
 import { Fragment, JsonFragment } from '@ethersproject/abi';
 import { ContractReceipt, ContractTransaction, ethers } from 'ethers';
+import ERC20Base from '../../abis/ERC20/ERC20Base.json';
+import ERC721Base from '../../abis/ERC721/ERC721Base.json';
+import { ContractType } from '../types';
 
 export async function processTransaction(
   tx: ContractTransaction
@@ -8,10 +11,20 @@ export async function processTransaction(
   return receipt;
 }
 
-export function parseErrorData(
-  error: any,
-  abi: string | readonly (string | JsonFragment | Fragment)[]
-) {
+export function parseErrorData(error: any, contractType: ContractType) {
+  let abi: any;
+
+  switch (contractType) {
+    case ContractType.ERC721:
+      abi = ERC721Base.abi;
+      break;
+    case ContractType.ERC20:
+      abi = ERC20Base.abi;
+      break;
+  }
+
+  if (!abi) throw new Error("Can't determine ABI");
+
   const iface = new ethers.utils.Interface(abi);
 
   //@TODO: Needs a refactor.
