41eca44ebcb299e64782e8e7972f0ce73cf00039
Andy <andy@openformat.tech>
2023-04-12 08:10:59 +0100

    [react] update provider and wagmi client to conform to new network structure

--- packages/react/src/wagmi-client.ts
+++ packages/react/src/wagmi-client.ts
@@ -1,56 +1,36 @@
 import { Chain, configureChains, createClient } from 'wagmi';
-import { foundry, polygon, polygonMumbai } from 'wagmi/chains';
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet';
 import { InjectedConnector } from 'wagmi/connectors/injected';
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
-import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 
-//@TODO we need to create a custom chain because we're using an older version of wagmi
-//which doesn't export aurora. Updating wagmi is a bigger piece of work,
-// so this is a temporary work around.
+export function wagmiClient(acceptedChains: Chain[]) {
+  const { chains, provider, webSocketProvider } = configureChains(
+    [...acceptedChains],
+    [
+      jsonRpcProvider({
+        rpc: (chain) => {
+          return chain.rpcUrls.default.http;
+        },
+      }),
+    ]
+  );
 
-export const aurora = {
-  id: 1313161554,
-  name: 'Aurora',
-  network: 'aurora',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'ETH',
-    symbol: 'ETH',
-  },
-  rpcUrls: {
-    public: 'https://mainnet.aurora.dev',
-    default: 'https://mainnet.aurora.dev',
-  },
-} as const satisfies Chain;
+  const metamask = new MetaMaskConnector({ chains });
 
-const { chains, provider, webSocketProvider } = configureChains(
-  [foundry, polygon, polygonMumbai, aurora],
-  [publicProvider()]
-);
+  const walletconnect = new WalletConnectConnector({
+    chains,
+    options: {
+      qrcode: true,
+    },
+  });
 
-const metamask = new MetaMaskConnector({ chains });
+  const injected = new InjectedConnector({ chains });
 
-const walletconnect = new WalletConnectConnector({
-  chains,
-  options: {
-    qrcode: true,
-  },
-});
-
-const coinbase = new CoinbaseWalletConnector({
-  chains,
-  options: {
-    appName: 'Open Format',
-  },
-});
-
-const injected = new InjectedConnector({ chains });
-
-export const wagmiClient = createClient({
-  autoConnect: false,
-  connectors: [injected, coinbase, metamask, walletconnect],
-  provider,
-  webSocketProvider,
-});
+  return createClient({
+    autoConnect: false,
+    connectors: [injected, metamask, walletconnect],
+    provider,
+    webSocketProvider,
+  });
+}
