afc4b41f3ad54f963a78e3553b31e5be3692dd41
Andy <andy@openformat.tech>
2023-04-18 15:22:35 +0100

    [sdk] create merge abi helper, add ContractError enums

---
+++ packages/sdk/src/constants/contracts.ts
@@ -0,0 +1,21 @@
+import { unionBy } from 'lodash';
+import ERC721LazyDropFacet from '../../abis/facet/ERC721LazyDropFacet.json';
+import RewardFacet from '../../abis/facet/RewardFacet.json';
+import SettingsFacet from '../../abis/facet/SettingsFacet.json';
+import Factory from '../../abis/Factory/Factory.json';
+import Proxy from '../../abis/proxy/Proxy.json';
+import ERC20Base from '../../abis/tokens/ERC20/ERC20Base.json';
+import ERC721Base from '../../abis/tokens/ERC721/ERC721Base.json';
+import ERC721LazyMint from '../../abis/tokens/ERC721/ERC721LazyMint.json';
+
+export const abis = unionBy(
+  Proxy,
+  ERC20Base,
+  SettingsFacet,
+  RewardFacet,
+  ERC721Base,
+  Factory,
+  ERC721LazyDropFacet,
+  ERC721LazyMint,
+  'name'
+);
