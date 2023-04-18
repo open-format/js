2ca5672506e0523f22fe1cf28a5cf7fdc6c8a05d
Andy <andy@openformat.tech>
2023-04-18 15:34:25 +0100

    [sdk] update error helper

--- packages/sdk/src/helpers/transaction.ts
+++ packages/sdk/src/helpers/transaction.ts
@@ -1,11 +1,12 @@
-import { ContractReceipt, ContractTransaction, ethers } from 'ethers';
-import ERC721LazyDropFacet from '../../abis/facet/ERC721LazyDropFacet.json';
-import SettingsFacet from '../../abis/facet/SettingsFacet.json';
-import Factory from '../../abis/Factory/Factory.json';
-import ERC20Base from '../../abis/tokens/ERC20/ERC20Base.json';
-import ERC721Base from '../../abis/tokens/ERC721/ERC721Base.json';
-import ERC721LazyMint from '../../abis/tokens/ERC721/ERC721LazyMint.json';
-import { ContractType } from '../types';
+import {
+  BytesLike,
+  ContractReceipt,
+  ContractTransaction,
+  ethers,
+} from 'ethers';
+import { forOwn, isObject } from 'lodash';
+import { abis } from '../constants/contracts';
+import { ContractErrors } from '../types';
 
 export async function processTransaction(
   tx: ContractTransaction
@@ -14,47 +15,44 @@ export async function processTransaction(
   return receipt;
 }
 
-export function parseErrorData(error: any, contractType: ContractType) {
-  let abi: any;
+export function parseErrorData(error: any) {
+  const iface = new ethers.utils.Interface(abis);
+  let result: BytesLike = '';
 
-  switch (contractType) {
-    case ContractType.NFT:
-      abi = ERC721Base.abi;
-      break;
-    case ContractType.NFTLazyMint:
-      abi = ERC721LazyMint.abi;
-      break;
-    case ContractType.NFTDrop:
-      abi = ERC721LazyDropFacet.abi;
-      break;
-    case ContractType.Token:
-      abi = ERC20Base.abi;
-      break;
-    case ContractType.Settings:
-      abi = SettingsFacet.abi;
-      break;
-    case ContractType.Factory:
-      abi = Factory.abi;
-      break;
+  function getSignatureHash(error: any) {
+    forOwn(error, (value, key) => {
+      if (isObject(value)) {
+        getSignatureHash(value);
+      } else if (key === 'data') {
+        if (value.length === 10) {
+          result = value;
+        }
+      }
+    });
   }
 
-  if (!abi) throw new Error("Can't determine ABI");
+  getSignatureHash(error);
 
-  const iface = new ethers.utils.Interface(abi);
+  interface ContractErrorsInterface {
+    [key: string]: number;
+  }
 
-  //@TODO: Needs a refactor. What happens if the error is on the Proxy?
-  // Must return: error signature, name, transaction data
+  const contractErrorsObj =
+    ContractErrors as unknown as ContractErrorsInterface;
 
-  if (error?.data) {
-    if (iface.parseError(error.data)) return iface.parseError(error.data).name;
-  } else if (error?.error?.error?.error?.data) {
-    if (iface.parseError(error.error.error.error.data))
-      return iface.parseError(error.error.error.error.data).name;
-  } else if (error?.reason === 'network does not support ENS') {
-    return 'Invalid wallet address';
-  } else if (error?.reason) {
-    return error.reason;
-  } else {
-    return error;
+  try {
+    if (result) {
+      const SigName = iface.parseError(result).name;
+
+      if (SigName in contractErrorsObj) {
+        return contractErrorsObj[SigName];
+      } else {
+        return 'SigHash not found in ContractErrors type';
+      }
+    } else {
+      return error;
+    }
+  } catch (e: any) {
+    throw Error(e);
   }
 }
