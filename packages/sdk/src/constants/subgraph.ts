f3489194bc85b3846567bc3ea2e88724e86f699d
Andy <andy@openformat.tech>
2023-04-25 08:44:46 +0100

    [sdk] fix aurora testnet subgraph endpoint

--- packages/sdk/src/constants/subgraph.ts
+++ packages/sdk/src/constants/subgraph.ts
@@ -18,7 +18,7 @@ export const Subgraphs: SubgraphData = {
     url: 'https://api.thegraph.com/subgraphs/name/open-format/aurora',
   },
   [Chains.auroraTestnet.id]: {
-    url: 'https://api.thegraph.com/subgraphs/name/open-format/auroraTestnet',
+    url: 'https://api.thegraph.com/subgraphs/name/open-format/aurora-testnet',
   },
   [Chains.foundry.id]: {
     url: 'http://0.0.0.0:8000/subgraphs/name/open-format/mumbai',
