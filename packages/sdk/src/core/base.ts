ed0e7b799559af27108e35fdace75a54cd34950e
Andy <andy@openformat.tech>
2023-06-01 13:24:36 +0100

    Add gas helper functions

--- packages/sdk/src/core/base.ts
+++ packages/sdk/src/core/base.ts
@@ -1,5 +1,4 @@
-import axios from 'axios';
-import { ethers, providers, Signer } from 'ethers';
+import { BigNumber, ethers, providers, Signer } from 'ethers';
 import { Chains } from '../constants';
 import { getPolygonGasFee } from '../helpers';
 import { getSubgraphUrlFromChainID } from '../helpers/providers';
@@ -54,33 +53,41 @@ export class BaseContract {
    *
    * @throws Will throw an error if the network provider is not available or there's an issue with fetching gas data.
    */
-  protected async getGasPrice() {
+  protected async getGasPrice(): Promise<{
+    maxFeePerGas?: ethers.BigNumber;
+    maxPriorityFeePerGas?: ethers.BigNumber;
+    gasPrice?: ethers.BigNumber;
+  }> {
     const feeData = await this.provider.getFeeData();
     const isEIP559 = feeData.maxFeePerGas && feeData.maxPriorityFeePerGas;
 
     if (isEIP559) {
       // EIP-1559 is supported
       const chainId = (await this.provider.getNetwork()).chainId;
+      let maxPriorityFeePerGas: BigNumber;
 
       if (
         chainId === Chains.polygon.id ||
         chainId === Chains.polygonMumbai.id
       ) {
-        const await getPolygonGasFee(chainId);
+        maxPriorityFeePerGas = await getPolygonGasFee(chainId);
+      } else {
+        maxPriorityFeePerGas = BigNumber.from(feeData.maxPriorityFeePerGas);
       }
 
       const block = await this.provider.getBlock('latest');
-      const maxPriorityFeePerGas = ethers.utils.parseUnits('1.0', 'gwei'); // Suggested miner tip
-      const maxFeePerGas = block.baseFeePerGas
-        ? block.baseFeePerGas.mul(2)
-        : maxPriorityFeePerGas; // Suggested max fee is twice the base fee
+      const baseBlockFee =
+        block && block.baseFeePerGas
+          ? block.baseFeePerGas
+          : ethers.utils.parseUnits('1', 'gwei');
 
+      const baseMaxFeePerGas = baseBlockFee.mul(2);
+      const maxFeePerGas = baseMaxFeePerGas.add(maxPriorityFeePerGas);
       return {
         maxFeePerGas,
         maxPriorityFeePerGas,
       };
     } else {
-      console.log('NOT EIP-1559');
       // For non EIP-1559 networks, use the standard gas price
       const gasPrice = await this.provider.getGasPrice();
 
