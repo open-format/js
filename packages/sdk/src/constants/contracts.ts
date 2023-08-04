6a5c59fe4daba14e0bace353a2ae40a1874d1bb3
Andy <andy@openformat.tech>
2023-08-04 09:20:04 +0100

    update constants

--- packages/sdk/src/constants/contracts.ts
+++ packages/sdk/src/constants/contracts.ts
@@ -2,7 +2,8 @@ import unionBy from 'lodash.unionby';
 import ERC721LazyDropFacet from '../../abis/facet/ERC721LazyDropFacet.json';
 import RewardFacet from '../../abis/facet/RewardFacet.json';
 import SettingsFacet from '../../abis/facet/SettingsFacet.json';
-import Factory from '../../abis/Factory/Factory.json';
+import ConstellationFactory from '../../abis/factories/ConstellationFactory.json';
+import StarFactory from '../../abis/factories/StarFactory.json';
 import Proxy from '../../abis/proxy/Proxy.json';
 import ERC20Base from '../../abis/tokens/ERC20/ERC20Base.json';
 import ERC721Base from '../../abis/tokens/ERC721/ERC721Base.json';
@@ -14,7 +15,8 @@ export const abis = unionBy(
   SettingsFacet,
   RewardFacet,
   ERC721Base,
-  Factory,
+  ConstellationFactory,
+  StarFactory,
   ERC721LazyDropFacet,
   ERC721LazyMint,
   'name'
