e86143545c141dfd996561b824e611b54e17f7d6
Andy <andy@openformat.tech>
2023-08-07 13:24:06 +0100

    refactor app tests

--- packages/sdk/test/app.test.ts
+++ packages/sdk/test/app.test.ts
@@ -1,42 +1,21 @@
-import { AppSetCreatorAccessParams, Chains, OpenFormatSDK } from '../src';
-import {
-  APP_ID,
-  PRIVATE_KEY,
-  WALLET_ADDRESS2,
-  WALLET_ADDRESS3,
-  ZERO_ADDRESS,
-} from './utilities';
+import { AppSetCreatorAccessParams } from '../src';
+import { WALLETS, ZERO_ADDRESS } from './utilities';
 
 describe('App', () => {
-  let sdk: OpenFormatSDK;
-  let walletAddress: string;
-
-  beforeAll(async () => {
-    sdk = new OpenFormatSDK({
-      network: Chains.foundry,
-      appId: APP_ID,
-      signer: PRIVATE_KEY,
-    });
-
-    if (sdk.signer) {
-      walletAddress = await sdk.signer?.getAddress();
-    }
-  });
-
   beforeEach(async () => {
-    await sdk.App.setCreatorAccess({
-      accounts: [ZERO_ADDRESS, walletAddress, WALLET_ADDRESS2, WALLET_ADDRESS3],
+    await global.sdk.App.setCreatorAccess({
+      accounts: [ZERO_ADDRESS, WALLETS[0], WALLETS[1], WALLETS[2]],
       approvals: [false, false, false, false],
     });
   });
 
   describe('setCreatorAccess()', () => {
     it('should by default only allow the owner to create contracts', async () => {
-      const hasCreatorAccess = await sdk.App.hasCreatorAccess({
-        account: WALLET_ADDRESS2,
+      const hasCreatorAccess = await global.sdk.App.hasCreatorAccess({
+        account: WALLETS[1],
       });
-      const hasCreatorAccessOwner = await sdk.App.hasCreatorAccess({
-        account: walletAddress,
+      const hasCreatorAccessOwner = await global.sdk.App.hasCreatorAccess({
+        account: WALLETS[0],
       });
 
       expect(hasCreatorAccess).toBe(false);
@@ -45,16 +24,16 @@ describe('App', () => {
       expect(hasCreatorAccessOwner).toBe(true);
     });
 
-    it(`gives ${WALLET_ADDRESS2} access to creator contracts`, async () => {
+    it(`gives ${WALLETS[1]} access to creator contracts`, async () => {
       const params: AppSetCreatorAccessParams = {
-        accounts: [WALLET_ADDRESS2],
+        accounts: [WALLETS[1]],
         approvals: [true],
       };
 
-      await sdk.App.setCreatorAccess(params);
+      await global.sdk.App.setCreatorAccess(params);
 
-      const hasCreatorAccess = await sdk.App.hasCreatorAccess({
-        account: WALLET_ADDRESS2,
+      const hasCreatorAccess = await global.sdk.App.hasCreatorAccess({
+        account: WALLETS[1],
       });
 
       expect(hasCreatorAccess).toBe(true);
@@ -66,10 +45,10 @@ describe('App', () => {
         approvals: [true],
       };
 
-      await sdk.App.setCreatorAccess(params);
+      await global.sdk.App.setCreatorAccess(params);
 
-      const hasCreatorAccess = await sdk.App.hasCreatorAccess({
-        account: WALLET_ADDRESS3,
+      const hasCreatorAccess = await global.sdk.App.hasCreatorAccess({
+        account: WALLETS[2],
       });
 
       expect(hasCreatorAccess).toBe(true);
@@ -78,7 +57,7 @@ describe('App', () => {
 
   describe('setAcceptedCurrencies()', () => {
     it('should accept ETH as acceptedCurrencies', async () => {
-      const tx = await sdk.App.setAcceptedCurrencies({
+      const tx = await global.sdk.App.setAcceptedCurrencies({
         currencies: [ZERO_ADDRESS],
         approvals: [true],
       });
