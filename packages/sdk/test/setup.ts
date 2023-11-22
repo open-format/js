b650c6448be738ac73e0c685b76ff641f94a5a9f
Andy <andy@openformat.tech>
2023-11-22 14:10:21 +0000

    jest globalSetup script is now .ts

--- packages/sdk/test/setup.ts
+++ packages/sdk/test/setup.ts
@@ -1,7 +1,13 @@
+require('ts-node').register({
+  transpileOnly: true,
+});
+
 import { faker } from '@faker-js/faker';
-import { ethers } from 'ethers';
-import { Chains, OpenFormatSDK, toWei } from '../src';
+import { ContractFactory, ethers } from 'ethers';
+import { Chains, OpenFormatSDK, toWei, txFeeContractAddress } from '../src';
+import { ERC20Base__factory } from '../src/contract-types';
 import { WALLETS } from './utilities';
+import ERC20Base from './utils/ERC20Base.json';
 
 // @dev NEVER commit non local private key. This key is the private key for anvil local node
 export const PRIVATE_KEY =
@@ -13,6 +19,19 @@ module.exports = async () => {
     starId: '',
     signer: PRIVATE_KEY,
   });
+
+  // Create $OPEN for transaction fee requestFee;
+  // Deploy the $OPEN token for transaction fees
+  const ERC20BaseFactory = new ContractFactory(
+    ERC20Base.abi,
+    ERC20Base.data.bytecode,
+    sdk.signer
+  );
+
+  const OPEN = await ERC20BaseFactory.deploy();
+  await OPEN.deployed();
+
+  txFeeContractAddress[31337] = { address: OPEN.address };
 
   if (sdk.signer) {
     global.walletAddress = await sdk.signer?.getAddress();
@@ -43,6 +62,13 @@ module.exports = async () => {
     signer: PRIVATE_KEY,
   });
 
+  const constellationInstance = ERC20Base__factory.connect(
+    constellationTx.constellationAddress,
+    global.sdk.signer
+  );
+
+  await constellationInstance.approve(global.star, ethers.constants.MaxUint256);
+
   // Create NFT
   const NFT = await global.sdk.App.createNFT({
     name: faker.science.chemicalElement().name,
@@ -61,12 +87,15 @@ module.exports = async () => {
 
   // Create Token
   const Token = await global.sdk.App.createToken({
-    name: 'TEST',
-    symbol: 'NFT',
-    supply: ethers.utils.parseEther('10'),
+    name: 'XP',
+    symbol: 'XP',
+    supply: toWei('100'),
   });
 
   global.NFT = NFT;
   global.NFTDrop = NFTDrop;
   global.Token = Token;
+  global.OPEN = OPEN;
+
+  console.log('Setup complete 🚀');
 };
