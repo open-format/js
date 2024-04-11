8ac36140c87a8e0d564de6cb17cbede3b032e4a4
Andy <andy@openformat.tech>
2024-04-11 08:58:52 +0100

    add new AppFactory abi to contracts abis constants

--- packages/sdk/src/constants/contracts.ts
+++ packages/sdk/src/constants/contracts.ts
@@ -2,8 +2,7 @@ import unionBy from 'lodash.unionby';
 import ERC721LazyDropFacet from '../../abis/facet/ERC721LazyDropFacet.json';
 import RewardsFacet from '../../abis/facet/RewardsFacet.json';
 import SettingsFacet from '../../abis/facet/SettingsFacet.json';
-import ConstellationFactory from '../../abis/factories/ConstellationFactory.json';
-import StarFactory from '../../abis/factories/StarFactory.json';
+import AppFactory from '../../abis/factories/AppFactory.json';
 import Proxy from '../../abis/proxy/Proxy.json';
 import ERC20Base from '../../abis/tokens/ERC20/ERC20Base.json';
 import ERC721Base from '../../abis/tokens/ERC721/ERC721Base.json';
@@ -16,8 +15,7 @@ export const abis = unionBy(
   RewardsFacet,
   // @ts-ignore
   ERC721Base,
-  ConstellationFactory,
-  StarFactory,
+  AppFactory,
   ERC721LazyDropFacet,
   ERC721LazyMint,
   'name'
